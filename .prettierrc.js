45ea0c711558f9a0ff083efe472321095fa8645a
pritesh-git <pritesh.bhati59@gmail.com>
2022-09-19 20:15:12 +0530

    format upgrade

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  endOfLine: 'crlf',
+}
